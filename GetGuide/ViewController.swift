7f7d4db66b78a9dd49367d8317c730d3afefd730
Vladislav Kapicyn <goodsnasey@gmail.com>
2021-03-29 15:09:52 +0300

    Initial Commit

---
+++ GetGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetGuide
+//
+//  Created by Владислав Капицын on 29.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
